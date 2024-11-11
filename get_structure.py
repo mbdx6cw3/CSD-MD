757ba38233712c5b1fc9a2a5eadb9840a4986585
Chris Williams <christopher.williams-2@manchester.ac.uk>
2024-11-11 18:55:11 +0000

    add functions for solvated / ionised ligands

--- get_structure.py
+++ get_structure.py
@@ -6,8 +6,7 @@ def ligand(identifier, simulation):
     import sys
     resetopenflags = sys.getdlopenflags()
     from ccdc.conformer import ConformerGenerator
-    from ccdc.io import MoleculeWriter
-    from ccdc.io import EntryReader
+    from ccdc.io import EntryReader, MoleculeWriter
     # this is required to reset paths after loading ccdc modules
     sys.setdlopenflags(resetopenflags)
 
@@ -24,14 +23,19 @@ def ligand(identifier, simulation):
     simulation.n_conf = len(conformers)
     simulation.conformers = [c.molecule for c in conformers]
 
+    # TODO: solvated systems required ionised ligands
+    # TODO: for now need to print pdb, ionise using OB and then write and fix pdb
+    if simulation.solvate:
+        for i_conf, c in enumerate(conformers):
+            with MoleculeWriter(f"{simulation.input_dir}ligand.pdb") as mol_writer:
+                mol_writer.write(c.molecule)
+        simulation.smiles = ionize_smiles(simulation.smiles)
+        ionize_pdb(f"{simulation.input_dir}ligand.pdb")
+
     if simulation.type == "enhanced":
         geometry_analysed_mol = get_torsions(conformers[0].molecule)
         simulation.unique_torsions = get_unique_torsions(geometry_analysed_mol)
 
-    # TODO: CCDC MoleculeWriter does not print MODEL or ENDMDL
-    # TODO: these keywords are used to look at different structures of the same molecule
-    # TODO: at present PDBFile sees only one structure with n_conf aspirins
-    # TODO: this problem will go away with conversion functions
     if simulation.analyse_torsions:
         print("Torsion Angle | Atom Indices")
         geometry_analysed_mol = get_torsions(conformers[0].molecule)
@@ -85,27 +89,6 @@ def docking():
     return None
 
 
-def fix_pdb(identifier, simulation):
-    """
-
-    :return:
-    """
-    from pdbfixer import PDBFixer
-    from openmm import app
-    fixer = PDBFixer(pdbid=identifier)
-    print("Fixing PDB file...")
-    fixer.findMissingResidues()
-    fixer.findNonstandardResidues()
-    fixer.replaceNonstandardResidues()
-    fixer.removeHeterogens(False)
-    fixer.findMissingAtoms()
-    fixer.addMissingAtoms()
-    fixer.addMissingHydrogens(7.0)
-    app.PDBFile.writeFile(fixer.topology, fixer.positions,
-        open(f"{simulation.input_dir}/protein.pdb", "w"))
-    return None
-
-
 def get_torsions(molecule):
     from ccdc.conformer import GeometryAnalyser
     engine = GeometryAnalyser()
@@ -132,3 +115,54 @@ def get_unique_torsions(molecule):
             unique_torsions.append(all_torsions[count].atom_indices)
     return unique_torsions
 
+
+def ionize_smiles(smiles):
+    from openbabel import openbabel
+    pH = 7.4
+    obConversion = openbabel.OBConversion()
+    obConversion.SetInAndOutFormats("smi", "smi")
+    mol = openbabel.OBMol()
+    obConversion.ReadString(mol, smiles)
+    mol.CorrectForPH(pH)
+    smiles = obConversion.WriteString(mol)
+    return smiles
+
+
+def ionize_pdb(filename):
+    from openbabel import openbabel
+    pH = 7.4
+    obConversion = openbabel.OBConversion()
+    obConversion.SetInAndOutFormats("pdb", "pdb")
+    mol = openbabel.OBMol()
+    obConversion.ReadFile(mol, filename)
+    mol.CorrectForPH(pH)
+    mol.AddHydrogens()
+    obConversion.WriteFile(mol, filename)
+    fix_ob_output(filename)
+    return None
+
+
+def fix_ob_output(filename):
+    '''
+    fix atom labelling
+    ---------
+    '''
+    with open(filename) as infile:
+        # count element in a dict
+        ele_count = {}
+        text = ""
+        for pdb_line in infile:
+            line = pdb_line.strip().split()
+            if line[0] == "HETATM":
+                ele = line[2]
+                try:
+                    # rename if more than one (add count)
+                    ele_count[ele] += 1
+                except KeyError:
+                    ele_count[ele] = 1
+                atom_name = ele + str(ele_count[ele])
+                pdb_line = pdb_line.replace(f"{ele}  ", f"{atom_name:<3}")
+            text = text + pdb_line
+    with open(filename, "w") as outfile:
+        outfile.write(text)
+    return None
